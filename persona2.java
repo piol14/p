d57a1621ee6d6bdf7530e2b2ef10863ca8ecac8e
macanyiz@gmail.com <macanyiz@gmail.com>
2023-05-23 20:58:00 +0200

    HE hecho cambios

--- persona2.java
+++ persona2.java
@@ -6,3 +6,9 @@ this.nombre = nombre;
 this.edad = edad;
 }
 //getters and setters
+
+public void imprimirDatos() {
+System.out.println("Nombre: " +
+nombre);
+System.out.println("Edad: " + edad);
+}
